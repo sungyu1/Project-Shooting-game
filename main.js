6d71f7983ab5d75a34c15eee8c4e710134a5e328
선규 <106080105+sungyu1@users.noreply.github.com>
2023-01-13 18:59:23 +0900

    js: 적군생성 및 GameOver

--- main.js
+++ main.js
@@ -13,26 +13,56 @@ document.body.appendChild(canvas);
 //  가져올이미지 정의
 let backgroundImage,spaceshipImg,bulletImage,enemyImage,gameOverImage;
 
+// Game Over
+let gameOver=false; //true 이면 게임이 끝난다.false 이면 게임이 계속된다.
+
 // 우주선 좌표 계속바뀌기 때문에 전역변수로 
 let spaceshipX =canvas.width/2-24;
 let spaceshipY =canvas.height-48;
 
 let bulletList=[] //총알 저장 리스트
+let enemyList=[]  //적군 저장 리스트
 
 // 총알 좌표 
 function Bullet(){
   this.x=0;
   this.y=0;
   this.init=function(){
-    this.x =spaceshipX+7;
+    this.x =spaceshipX+7; // 7은 우주선 중앙에서 미사일 발사
     this.y=spaceshipY
-
-    bulletList.push(this)
+    
+    bulletList.push(this);
   };
   //총알 발사
-this.update=function(){
-  this.y-=7;
-};
+  this.update=function(){
+    this.y-=7;
+  };
+}
+
+// 적군을 만들기 위한 랜덤 함수
+function generateRandomValue(min,max){
+  let randomNum= Math.floor(Math.random()*(max-min+1))+min
+  return randomNum;
+}
+
+// 적군좌표
+function Enemy(){
+  this.x=0;
+  this.y=0;
+  this.init=function(){
+    this.y=0;
+    this.x=generateRandomValue(0,canvas.width-64)
+    enemyList.push(this);
+  };
+  // 적군 내려오는 함수
+  this.update=()=>{
+    this.y += 2; //적군의 속도 조절
+
+    if(this.y >= canvas.height-64){
+      gameOver=true;
+      console.log("gameover")
+    }
+  }
 }
 
 
@@ -74,12 +104,20 @@ document.addEventListener("keyup",(event)=>{
   }
 });
  }
-
+// 총알생성
 function createBullet(){
   console.log('총알생성');
   let b= new Bullet(); //총알 하나생성
   b.init()
   console.log('새로운 총알리스트:',bulletList);
+}
+
+// 적군생성
+function createEnemy(){
+  const interval = setInterval(()=>{
+    let e=new Enemy();
+    e.init();
+  },1500)    // setInterval(호출하고싶은함수,시간)
 }
 
 function update(){
@@ -100,10 +138,11 @@ function update(){
     for(let i=0;i<bulletList.length;i++){
       bulletList[i].update();
     }
+    // 적군 내려오는 함수호출
+    for(let i=0;i<enemyList.length;i++){
+      enemyList[i].update();
+    }
   }
-
-
-
 
 
 
@@ -116,18 +155,26 @@ function render(){
   for(let i=0;i<bulletList.length;i++){
     ctx.drawImage(bulletImage,bulletList[i].x,bulletList[i].y);
   }
+  // 적군
+  for(let i=0;i<enemyList.length;i++){
+    ctx.drawImage(enemyImage,enemyList[i].x,enemyList[i].y);
+  }
 
 }
 function main(){
-  update();    //좌표값 업데이트함수 
-  render();     //그려주는 함수
-  requestAnimationFrame(main);
+  if(!gameOver){
+
+    update();  //좌표값 업데이트함수 
+    render();  //그려주는 함수
+    requestAnimationFrame(main);
+  }
 }
 
 
-// 함수사용(도구 만들어서 쓰는공간)
+// 함수사용(도구 만들어서 쓰는공간) 시작되는 시점
 loadImage();
 setupkeyboardListener();
+createEnemy();
 main();
 
 // 총알만들기
@@ -136,3 +183,9 @@ main();
 // 발사된 총알들은 총알 배열에 저장한다.
 // 총알들은 X,Y 좌표값이 있어야한다.
 // 총알 배열을 가지고 reandr을 한다.
+
+// 적군 생성하기 x,y 좌표, init ,update
+// 적군 위치는 1초마다 랜덤하게 나온다
+// 적군은 위에서 아래로 내려온다.
+// 바닥에 닿으면 Game over.
+// 적군과 총알이 만나면 적군이 사라진다. 그리고 점수 1점 획득.
